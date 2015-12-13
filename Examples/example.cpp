896be60d2f6ecccb40f58bc6bd7cdd5c13e5ac1a
bnaegel <b.naegel@unistra.fr>
2015-12-13 15:44:16 +0100

    BN

--- Examples/example.cpp
+++ Examples/example.cpp
@@ -3,7 +3,7 @@
 // headers pour les classes de base Image et FlatSE 
 #include "Common/Image.h"
 #include "Common/FlatSE.h"
-// header pour les algos principaux de morphologie (éro, dil, ouv, fer, rec,...)
+// header pour les algos principaux de morphologie (Ã©ro, dil, ouv, fer, rec,...)
 #include "Algorithms/Morphology.h"
 // header pour l'algo du Watershed
 #include "Algorithms/Watershed.h"
@@ -23,38 +23,38 @@ int main(int argc, char *argv[])
 		std::cout << "Usage: " << argv[0] << " <image.pgm> <h>\n";
 		exit(1);
 		}
-	// Image est une classe générique paramétrée par le type des points contenus dans l'image
+	// Image est une classe gÃ©nÃ©rique paramÃ©trÃ©e par le type des points contenus dans l'image
 	Image <unsigned char> im;
   	if(Image<U8>::load(argv[1],im))
 		std::cout <<"Great, image is loaded\n";
 	else return 1;
 	
-	// FlatSE est la classe stockant un élément structurant
+	// FlatSE est la classe stockant un Ã©lÃ©ment structurant
 	FlatSE connexity;
-	// initialisation de l'élément structurant 'connexity' à un 8-voisinage (l'élément ne contient pas l'origine)
+	// initialisation de l'Ã©lÃ©ment structurant 'connexity' Ã  un 8-voisinage (l'Ã©lÃ©ment ne contient pas l'origine)
 	//   X X X
 	//   X . X
 	//   X X X
 	connexity.make2DN8();
 	
 	FlatSE se;
-	// initialisation à un 9-voisinage (l'élément contient l'origine)
+	// initialisation Ã  un 9-voisinage (l'Ã©lÃ©ment contient l'origine)
 	// X X X
 	// X X X
 	// X X X 
 	se.make2DN9();
 	
 	////////////////////////////////////////////////////
-	// exemple érosion/dilatation                     //
+	// exemple Ã©rosion/dilatation                     //
 	////////////////////////////////////////////////////
 	
-	// érosion de l'image im par l'élément structurant se
+	// Ã©rosion de l'image im par l'Ã©lÃ©ment structurant se
 	Image <U8> imEro=erosion(im, se);
 	
-	// dilatation de l'image im par l'élément structurant se
+	// dilatation de l'image im par l'Ã©lÃ©ment structurant se
 	Image <U8> imDil=dilation(im, se);
 
-	// écriture des images résultat au format .pgm
+	// Ã©criture des images rÃ©sultat au format .pgm
 	imEro.save("imEro.pgm");
 	imDil.save("imDil.pgm");
 	
@@ -66,30 +66,30 @@ int main(int argc, char *argv[])
 	Image <U8> grad=morphologicalGradient(im,se);
 	grad.save("gradBeforeFiltering.pgm");
 	
-	// filtre h-min: suppression des minima du gradient non significatifs (évite la sur-segmentation du watershed)
+	// filtre h-min: suppression des minima du gradient non significatifs (Ã©vite la sur-segmentation du watershed)
 	
-	// paramètre h déterminant la profondeur maximale des minima éliminés
-	// possibilité d'obtenir une pyramide d'images pour chaque h
+	// paramÃ¨tre h dÃ©terminant la profondeur maximale des minima Ã©liminÃ©s
+	// possibilitÃ© d'obtenir une pyramide d'images pour chaque h
 	int h=atoi(argv[2]);
 	
 	hMinFilter(grad,se,h);
 	grad.save("gradAfterFiltering.pgm");
 	
-	// calcul des minima régionaux du gradient filtré
+	// calcul des minima rÃ©gionaux du gradient filtrÃ©
 	Image <U8> minima=regionalMinima(grad, connexity);
 	
 	// labelisation des minima
 	Image <TLabel> minimaLabel1=labelConnectedComponents(minima, connexity);
 	Image <TLabel> minimaLabel2=minimaLabel1;
 	
-	// Algo 1: watershed, algo de Meyer (le résultat est stocké dans l'image Label minimaLabel1)
+	// Algo 1: watershed, algo de Meyer (le rÃ©sultat est stockÃ© dans l'image Label minimaLabel1)
 	watershedMeyer(grad,minimaLabel1,connexity);
 	
-	// Algo 2: croissance de régions (avantage: calculé directement sur l'image originale)
+	// Algo 2: croissance de rÃ©gions (avantage: calculÃ© directement sur l'image originale)
 	seededRegionGrowing(im,minimaLabel2,connexity);
 	
-	// pour chaque région labélisée, calcule la moyenne de la région dans l'image originale
-	// création d'une image mosaïque
+	// pour chaque rÃ©gion labÃ©lisÃ©e, calcule la moyenne de la rÃ©gion dans l'image originale
+	// crÃ©ation d'une image mosaÃ¯que
 	
 	Image <U8> result1=computeMarkerMean(im, minimaLabel1);
 	Image <U8> result2=computeMarkerMean(im, minimaLabel2);
